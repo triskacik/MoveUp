0555bca3069fe8e381b9d2bd262d5adf43dcfabb
triskacik <treskon.tomas@gmail.com>
2020-03-29 12:09:33 +0200

    MoveUp Project Created, SummaryView WIP

---
+++ MoveUp/MoveUp/MoveUp/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Xamarin.Forms;
+using MoveUp.Views;
+using Xamarin.Forms.Xaml;
+
+namespace MoveUp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new SummaryView();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
